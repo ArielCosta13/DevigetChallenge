a678a2e7f79d4e7a80e7cde91b41621654890464
Ariel Costa <ariel.costa@web.com>
2019-10-25 08:14:01 -0300

    Search test now navigates to the second result page  -It was needed to use some javascript code in order to scroll down Javascript was used because the Actions commands provided by Webdriver wasnt reliable enough Assertions checks that there is an element in the results of the second page of the search result that has Iphone in its text and that is different from the element in the first page

--- src/test/java/com/deviget/automation/uitests/SearchTest.java
+++ src/test/java/com/deviget/automation/uitests/SearchTest.java
@@ -2,13 +2,17 @@ package com.deviget.automation.uitests;
 
 import com.deviget.automation.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class SearchTest extends BaseTest {
 
     @Test
-    public void iPhoneSearch(){
+    public void iPhoneSearch() throws InterruptedException {
         driver.navigate().to("https://www.aliexpress.com/");
         if (driver.findElement(By.cssSelector(".ui-window-content")).isDisplayed()){
             driver.findElement(By.cssSelector(".close-layer")).click();
@@ -16,8 +20,20 @@ public class SearchTest extends BaseTest {
         driver.findElement(By.cssSelector("#search-key")).sendKeys("IPhone");
         driver.findElement(By.cssSelector(".search-button")).click();
 
-        driver.findElement(By.xpath("//button[.='2']"));
-        System.out.println(driver.getTitle());
-        Assert.assertTrue(driver.getTitle().contains("AliExpress"));
+        List<WebElement> itemlist= driver.findElements(By.cssSelector(".list-item"));
+        System.out.println(itemlist.size());
+        String list1Item2 = itemlist.get(2).getText();
+        System.out.println(list1Item2);
+        Assert.assertTrue(list1Item2.toLowerCase().contains("iphone"));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", itemlist.get(itemlist.size()-1));
+        driver.findElement(By.xpath("//button[.='2']")).click();
+        Thread.sleep(5000);
+
+        List<WebElement> itemlist2= driver.findElements(By.cssSelector(".list-item"));
+        System.out.println(itemlist2.size());
+        System.out.println(itemlist2.get(2).getText());
+        Assert.assertTrue(itemlist2.get(2).getText().toLowerCase().contains("iphone"));
+        Assert.assertNotEquals(list1Item2,itemlist2.get(2).getText());
     }
 }
